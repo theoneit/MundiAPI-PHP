9cb641eea6bbdaa1612795f57c096a64f226ec57
Ian Lessa <ian.lessa1@gmail.com>
2019-05-30 11:22:13 -0300

    :sparkles: add cards examples

--- examples/card/create_card.php
+++ examples/card/create_card.php
@@ -1,3 +1,42 @@
 <?php
 
-//TODO
+$apiClient = new \MundiAPILib\MundiAPIClient(
+    $testSecretKey,
+    ''
+);
+
+$customerController = $apiClient->getCustomers();
+
+$customer = new \MundiAPILib\Models\CreateCustomerRequest();
+$customer->name = "sdk customer test";
+$customer->email = "tonystark@avengers.com";
+
+$request = new \MundiAPILib\Models\CreateCardRequest();
+
+$request->number = "4000000000000010";
+$request->holderName = "Tony Stark";
+$request->holderDocument = "93095135270";
+$request->expMonth = 1;
+$request->expYear = 25;
+$request->cvv = "351";
+// Brand is Optional field and autodetected;
+$request->brand = "Mastercard";
+$request->privateLabel = false;
+// Billing Address;
+$request->billingAddress = new \MundiAPILib\Models\CreateAddressRequest();
+$request->billingAddress->line1 = "10880, Malibu Point, Malibu Central";
+$request->billingAddress->line2 = "7º floor";
+$request->billingAddress->zipCode = "90265";
+$request->billingAddress->city = "Malibu";
+$request->billingAddress->state = "CA";
+$request->billingAddress->country = "US";
+// Card Options: Verify OneDollarAuth;
+$request->options = new \MundiAPILib\Models\CreateCardOptionsRequest();
+$request->options->verifyCard = true;
+
+$createdCustomer = $customerController->createCustomer($customer);
+$result = $customerController->createCard($createdCustomer->id, $request);
+
+return $result;
+    
+    
--- examples/card/get_cards.php
+++ examples/card/get_cards.php
@@ -1,3 +1,14 @@
 <?php
 
-//TODO
+$apiClient = new \MundiAPILib\MundiAPIClient(
+    $testSecretKey,
+    ''
+);
+
+$customerController = $apiClient->getCustomers();
+
+$customerId = "cus_G6gwy4xtJIOyNbrK";
+
+$result = $customerController->getCards($customerId, 1, 30);
+
+return $result;
