dacc815a0082c5ebe8212772dccedcad751b8def
Ian Lessa <ian.lessa1@gmail.com>
2019-05-30 11:07:57 -0300

    :sparkles: add create_address example

--- examples/address/create_address.php
+++ examples/address/create_address.php
@@ -1,3 +1,26 @@
 <?php
 
-//TODO
+$apiClient = new \MundiAPILib\MundiAPIClient(
+    $testSecretKey,
+    ''
+);
+
+$customerController = $apiClient->getCustomers();
+
+$customerId = "cus_PzRyp10FeNca2rVB";
+
+$request = new \MundiAPILib\Models\CreateAddressRequest();
+
+$request = new \MundiAPILib\Models\CreateAddressRequest();
+$request->line1 = "10880, Malibu Point, Malibu Central";
+$request->line2 = "7º floor";
+$request->zipCode = "90265";
+$request->city = "Malibu";
+$request->state = "CA";
+$request->country = "US";
+$request->metadata = new \MundiAPILib\Models\UpdateMetadataRequest();
+$request->metadata->id = "my_address_id";
+
+$result = $customerController->createAddress($customerId, $request);
+
+return $result;
