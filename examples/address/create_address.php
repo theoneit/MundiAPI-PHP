6ddf8c3af6701a01468f5f85c82a13f824d10867
Ian Lessa <ian.lessa1@gmail.com>
2019-05-30 14:04:55 -0300

    :memo: add example file headers

--- examples/address/create_address.php
+++ examples/address/create_address.php
@@ -1,4 +1,6 @@
 <?php
+//This example was built using MundiAPI-PHP SDK.
+//For more information, please refer to https://github.com/mundipagg/MundiAPI-PHP
 
 $secretKey = 'YOUR SECRET KEY'; //the secret key will be provided by MundiPagg.
 $basicAuthPassword = ''; //fill it with an empty string
--- examples/order/get_order_by_id.php
+++ examples/order/get_order_by_id.php
@@ -1,4 +1,6 @@
 <?php
+//This example was built using MundiAPI-PHP SDK.
+//For more information, please refer to https://github.com/mundipagg/MundiAPI-PHP
 
 $secretKey = 'YOUR SECRET KEY'; //the secret key will be provided by MundiPagg.
 $basicAuthPassword = ''; //fill it with an empty string
@@ -10,6 +12,8 @@ $apiClient = new \MundiAPILib\MundiAPIClient(
 
 $orderController = $apiClient->getOrders();
 
-$result = $orderController->getOrder("or_ExAmPlExxxxxxxxx);
+$orderId = "or_ExAmPlExxxxxxxxx";
+
+$result = $orderController->getOrder($orderId);
 
 return $result;
