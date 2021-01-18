1f77a2c14ca7db0b14db7a4558f4745304b2675f
Marcelo Marini <marcelomarini17@hotmail.com>
2021-01-18 17:34:58 +0000

    :art: Add recipient transfer_settings response

--- src/Models/GetSplitResponse.php
+++ src/Models/GetSplitResponse.php
@@ -49,21 +49,30 @@ class GetSplitResponse implements JsonSerializable
     public $options;
 
     /**
+     * @todo Write general description for this property
+     * @required
+     * @var string $id public property
+     */
+    public $id;
+
+    /**
      * Constructor to set initial or default values of member properties
      * @param string                  $type      Initialization value for $this->type
      * @param integer                 $amount    Initialization value for $this->amount
      * @param GetRecipientResponse    $recipient Initialization value for $this->recipient
      * @param string                  $gatewayId Initialization value for $this->gatewayId
      * @param GetSplitOptionsResponse $options   Initialization value for $this->options
+     * @param string                  $id        Initialization value for $this->id
      */
     public function __construct()
     {
-        if (5 == func_num_args()) {
+        if (6 == func_num_args()) {
             $this->type      = func_get_arg(0);
             $this->amount    = func_get_arg(1);
             $this->recipient = func_get_arg(2);
             $this->gatewayId = func_get_arg(3);
             $this->options   = func_get_arg(4);
+            $this->id        = func_get_arg(5);
         }
     }
 
@@ -79,6 +88,7 @@ class GetSplitResponse implements JsonSerializable
         $json['recipient']  = $this->recipient;
         $json['gateway_id'] = $this->gatewayId;
         $json['options']    = $this->options;
+        $json['id']         = $this->id;
 
         return $json;
     }
