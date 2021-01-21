91d09cf419aaaf82ab68680bc4b9ce00aeb9d782
Marcelo Marini <marcelomarini17@hotmail.com>
2021-01-21 21:20:50 +0000

    :art: Add document_type field

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -24,7 +24,7 @@ class BaseController
      * User-agent to be sent with API calls
      * @var string
      */
-    const USER_AGENT = 'MundiSDK - PHP 0.16.18';
+    const USER_AGENT = 'MundiSDK - PHP 0.16.19';
 
     /**
      * HttpCallBack instance associated with this controller
--- src/Models/CreateCustomerRequest.php
+++ src/Models/CreateCustomerRequest.php
@@ -77,29 +77,38 @@ class CreateCustomerRequest implements JsonSerializable
     public $gender;
 
     /**
+     * @todo Write general description for this property
+     * @maps document_type
+     * @var string|null $documentType public property
+     */
+    public $documentType;
+
+    /**
      * Constructor to set initial or default values of member properties
-     * @param string               $name     Initialization value for $this->name
-     * @param string               $email    Initialization value for $this->email
-     * @param string               $document Initialization value for $this->document
-     * @param string               $type     Initialization value for $this->type
-     * @param CreateAddressRequest $address  Initialization value for $this->address
-     * @param array                $metadata Initialization value for $this->metadata
-     * @param CreatePhonesRequest  $phones   Initialization value for $this->phones
-     * @param string               $code     Initialization value for $this->code
-     * @param string               $gender   Initialization value for $this->gender
+     * @param string               $name         Initialization value for $this->name
+     * @param string               $email        Initialization value for $this->email
+     * @param string               $document     Initialization value for $this->document
+     * @param string               $type         Initialization value for $this->type
+     * @param CreateAddressRequest $address      Initialization value for $this->address
+     * @param array                $metadata     Initialization value for $this->metadata
+     * @param CreatePhonesRequest  $phones       Initialization value for $this->phones
+     * @param string               $code         Initialization value for $this->code
+     * @param string               $gender       Initialization value for $this->gender
+     * @param string               $documentType Initialization value for $this->documentType
      */
     public function __construct()
     {
-        if (9 == func_num_args()) {
-            $this->name     = func_get_arg(0);
-            $this->email    = func_get_arg(1);
-            $this->document = func_get_arg(2);
-            $this->type     = func_get_arg(3);
-            $this->address  = func_get_arg(4);
-            $this->metadata = func_get_arg(5);
-            $this->phones   = func_get_arg(6);
-            $this->code     = func_get_arg(7);
-            $this->gender   = func_get_arg(8);
+        if (10 == func_num_args()) {
+            $this->name         = func_get_arg(0);
+            $this->email        = func_get_arg(1);
+            $this->document     = func_get_arg(2);
+            $this->type         = func_get_arg(3);
+            $this->address      = func_get_arg(4);
+            $this->metadata     = func_get_arg(5);
+            $this->phones       = func_get_arg(6);
+            $this->code         = func_get_arg(7);
+            $this->gender       = func_get_arg(8);
+            $this->documentType = func_get_arg(9);
         }
     }
 
@@ -110,15 +119,16 @@ class CreateCustomerRequest implements JsonSerializable
     public function jsonSerialize()
     {
         $json = array();
-        $json['name']     = $this->name;
-        $json['email']    = $this->email;
-        $json['document'] = $this->document;
-        $json['type']     = $this->type;
-        $json['address']  = $this->address;
-        $json['metadata'] = $this->metadata;
-        $json['phones']   = $this->phones;
-        $json['code']     = $this->code;
-        $json['gender']   = $this->gender;
+        $json['name']          = $this->name;
+        $json['email']         = $this->email;
+        $json['document']      = $this->document;
+        $json['type']          = $this->type;
+        $json['address']       = $this->address;
+        $json['metadata']      = $this->metadata;
+        $json['phones']        = $this->phones;
+        $json['code']          = $this->code;
+        $json['gender']        = $this->gender;
+        $json['document_type'] = $this->documentType;
 
         return $json;
     }
